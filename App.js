1869206071008c129227773a3ff85838301b3794
chintan.thakkar <201701141@daiict.ac.in>
2023-08-01 21:01:07 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>I am h1 tag</h1>
+ *          <h2>I am h2 tag</h2>
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+    "div", { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag"),
+    ])
+);
+
+// const heading = React.createElement(
+//     "h1", { id: "heading" },
+//     "Hello World from React!"
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
