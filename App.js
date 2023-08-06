fa60dc9ec101abc8e753abedeecb796f7d2e7672
chintan.thakkar <201701141@daiict.ac.in>
2023-08-06 11:11:26 +0530

    Episode 2: Added React in our code

--- App.js
+++ App.js
@@ -7,13 +7,19 @@
  * </div>
  */
 
-const parent = React.createElement(
-    "div", { id: "parent" },
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "This is Namaste React 🚀"),
+        React.createElement("h2", {}, "I am an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
         React.createElement("h1", {}, "I am an h1 tag"),
         React.createElement("h2", {}, "I am an h2 tag"),
-    ])
-);
+    ]),
+]);
 
 // const heading = React.createElement(
 //     "h1", { id: "heading" },
